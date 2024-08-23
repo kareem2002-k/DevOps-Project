94b4dc7fcb0c27142650b93bd50d8f7effdbe233
kareem2002-k <kareemsamir166@gmail.com>
2024-08-23 22:08:38 +0300

    Update

--- terraform/main.tf
+++ terraform/main.tf
@@ -1,36 +1,20 @@
-resource "aws_security_group" "allow_http_ssh" {
-  name        = "allow_http_ssh"
-  description = "Allow HTTP and SSH traffic"
-
-  ingress {
-    from_port   = 22
-    to_port     = 22
-    protocol    = "tcp"
-    cidr_blocks = ["0.0.0.0/0"]
-  }
-
-  ingress {
-    from_port   = 80
-    to_port     = 80
-    protocol    = "tcp"
-    cidr_blocks = ["0.0.0.0/0"]
-  }
-
-  egress {
-    from_port   = 0
-    to_port     = 0
-    protocol    = "-1"
-    cidr_blocks = ["0.0.0.0/0"]
-  }
+provider "aws" {
+  region = "us-east-1"
 }
 
-resource "aws_instance" "demo" {
-  ami             = var.ami
-  instance_type   = var.instance_type
-  key_name        = var.key_name
-  vpc_security_group_ids = [aws_security_group.allow_http_ssh.name]
+resource "aws_key_pair" "my_key" {
+  key_name   = "my_key"
+  public_key = file("my_key.pub")  # Ensure this path is correct
+}
+
+
+resource "aws_instance" "web" {
+  count         = 3
+  ami           = "ami-04a81a99f5ec58529"  # Replace with your AMI ID
+  instance_type = "t2.micro"
+  key_name      = aws_key_pair.my_key.key_name
 
   tags = {
-    Name = "demo"
+    Name = "WebInstance-${count.index}"
   }
 }
